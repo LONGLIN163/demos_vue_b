8ffd64d8eac3c23154fd22839c9903778aa3c62b
developerlin123 <developerlin123@gmail.com>
2021-09-16 15:22:26 +0200

    redirect and alias

---
+++ b05_vueRouter_redirect&alias/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <br>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/home2">Home2</router-link> |
+    <router-link to="/params/666/hahahhahaha">Params</router-link> |
+    <router-link to="/gohome">gohome</router-link> |
+    <router-link to="/goParams/888/xixixixixi">goParams</router-link> 
+    <router-link to="/Haha1">Haha1</router-link> 
+    <router-link to="/asdfasdfasd">asdfasdfasd</router-link> 
+
+    <router-view/>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ b05_vueRouter_redirect&alias/src/components/Params.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="haha">
+        <h3>{{message}}</h3>
+        <p>id:{{$route.params.newsid}}</p>
+        <p>title:{{$route.params.newstitle}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Haha1',
+        data() {
+            return {
+                message:"params page here"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+  .haha{
+      color:red
+  }
+</style>
