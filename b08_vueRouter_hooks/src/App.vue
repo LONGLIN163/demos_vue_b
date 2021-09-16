309ae5b75e351e74144119dcb1db551d51f2482f
developerlin123 <developerlin123@gmail.com>
2021-09-16 18:49:24 +0200

    hooks or gards

---
+++ b08_vueRouter_hooks/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <br>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/params/666/hahahhahaha">Params</router-link> |
+    <router-link to="/gohome">gohome</router-link> |
+    <router-link to="/goParams/888/xixixixixi">goParams</router-link> 
+    <router-link to="/Haha1">Haha1</router-link> 
+    <router-link to="/asdfasdfasd">asdfasdfasd</router-link> 
+
+    <transition name="fade" model="out-in">
+      <router-view/>
+    </transition >
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  .fade-enter {
+    opacity:0;
+  }
+  .fade-leave{
+    opacity:1;
+  }
+  .fade-enter-active{
+    transition:opacity .5s;
+  }
+  .fade-leave-active{
+    opacity:0;
+    transition:opacity .5s;
+  }
+</style>
---
+++ b08_vueRouter_hooks/src/router/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import Haha1 from '@/components/Haha1'
+import Haha2 from '@/components/Haha2'
+import Params from '@/components/Params'
+import PageNotFound from '@/components/Errors/PageNotFound'
+
+Vue.use(Router)
+
+export default new Router({
+  //mode:"history",
+  //mode:"hash",
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello,
+    },{
+      path: '/params/:newsid(\\d+)/:newstitle',
+      component:Params,
+      // beforeRouteEnter:((to, from, next) => {
+      //   /* must call `next` */
+      //   console.log(to)
+      //   console.log(from)
+      //   console.log(next)
+      //   //next()
+      //   //next(false)
+      //   next({
+      //     path:"/"
+      //   })
+      // })
+    },{
+      path:'/gohome',
+      redirect:'/'
+    },{
+      path:'/goParams/:newsid(\\d+)/:newstitle',
+      redirect:'/params/:newsid(\\d+)/:newstitle'
+    },{
+      path: '/Haha1',
+      component:Haha1,
+      alias:"/asdfasdfasd"
+    },{
+      path: '*',
+      component:PageNotFound
+    }
+  ]
+})
