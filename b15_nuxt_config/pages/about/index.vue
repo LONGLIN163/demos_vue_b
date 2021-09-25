5f617020734d6c222572187374ea6d94485bfe96
developerlin123 <developerlin123@gmail.com>
2021-09-25 16:42:16 +0200

    nuxt config and static router

---
+++ b15_nuxt_config/pages/about/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>about page</h1>
+        <ul>
+            <li><a href="/">home</a></li>
+        </ul>
+    </div>
+</template>
---
+++ b15_nuxt_config/pages/news/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>news page</h1>
+        <p>id:{{$route.params.newsId}}</p>
+        <ul>
+            <li><a href="/">home</a></li>
+        </ul>
+    </div>
+</template>
