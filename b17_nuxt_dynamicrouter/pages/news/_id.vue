07660fa2352761803c114c1c9a352c951e324919
developerlin123 <developerlin123@gmail.com>
2021-09-25 17:59:51 +0200

    dynamic router and animations

---
+++ b17_nuxt_dynamicrouter/pages/news/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+       <h2>News-Content : {{$route.params.id}}</h2>
+       <ul>
+        <!-- <li><a href="/">Home</a></li> -->
+          <li><Nuxt-link to="/">home</Nuxt-link></li>
+       </ul>
+  </div>
+</template> 
+<script>
+export default {
+    validate({params}) {
+        return /^\d+$/.test(params.id)
+    }
+}
+</script>
