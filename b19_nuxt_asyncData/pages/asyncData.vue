6c6ef0e0c6c48d6ba8a74b6faa805fd2bb2b7836
developerlin123 <developerlin123@gmail.com>
2021-09-27 11:45:50 +0200

    pull fake data methods test

---
+++ b19_nuxt_asyncData/pages/asyncData.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <h1>name:{{info.name}}</h1>
+      <h2>age:{{info.age}}</h2>
+      <h2>hobby:{{info.interest}}</h2>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  data(){
+     return {
+         name:'hello World',
+     }
+  },
+  // metod 1
+/*   asyncData(){
+      return axios.get('https://api.jsonserve.com/ioEACZ')
+      .then((res)=>{
+          console.log(res)
+          return {info:res.data}
+      })
+
+  } */
+
+  // metod 2
+    async asyncData(){
+        let {data}=await axios.get('https://api.jsonserve.com/ioEACZ')
+        return {info: data}
+    }
+}
+</script>
